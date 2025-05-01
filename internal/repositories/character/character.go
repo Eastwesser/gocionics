1f8a4227c3a6cc9aa55bc9d1365d68c490710d97
Денис Матвеев <alttert@mail.ru>
2025-05-02 00:10:21 +0300

    repositories for user and character upd

--- internal/repositories/character/character.go
+++ internal/repositories/character/character.go
@@ -1,1 +1,8 @@
 package character
+
+import "gocionics/internal/entities/character"
+
+type ICharacterRepository interface {
+	GetById(id int) (*character.Character, error)
+	ListAll() (*[]character.Character, error)
+}
--- internal/repositories/user/user.go
+++ internal/repositories/user/user.go
@@ -4,4 +4,6 @@ import "gocionics/internal/entities/user"
 
 type IUserRepository interface {
 	Create(user *user.User) (string, error)
+	GetById(id string) (user.User, error)
+	AssignCharacter(userID, characterID int) (string, error)
 }
