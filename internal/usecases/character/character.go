d45b4f1865b8b3ee63c61c7c7af5e2d033cb5421
Денис Матвеев <alttert@mail.ru>
2025-05-02 01:35:07 +0300

    added usecases for character

--- internal/usecases/character/character.go
+++ internal/usecases/character/character.go
@@ -1,1 +1,15 @@
 package character
+
+import "gocionics/internal/repositories/character"
+
+type CharacterUseCase struct {
+	repo character.ICharacterRepository
+}
+
+func NewCharacterUseCase(repo character.ICharacterRepository) *CharacterUseCase {
+	return &CharacterUseCase{repo: repo}
+}
+
+func (uc *CharacterUseCase) GetCharacter(id int) (*character.Character, error) {
+	return uc.repo.GetByID(id)
+}
